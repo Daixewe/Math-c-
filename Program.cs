4be750050b57d4a74719c9d299fdf6ba4633da5d
Daixewe <fuentesvd56@gmail.com>
2022-06-14 23:49:04 -0600

    Operaciones matematicas

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Math");
+
+//  Suma
+Console.WriteLine(56 + 12);
+Console.WriteLine(20 + 45);
+// Resta
+Console.WriteLine(13 - 12);
+Console.WriteLine(120 - 78);
+// Multiplicacion
+Console.WriteLine(10 * 24);
+Console.WriteLine(23 * 15);
+// Division
+Console.WriteLine(Decimal.Divide(6, 4));
+Console.WriteLine(Decimal.Divide(45, 12));
+// Exponente
+Console.WriteLine(Math.Pow(15, 2));
+Console.WriteLine(Math.Pow(45, 3));
+// Division entera
+Console.WriteLine(45 / 5);
+Console.WriteLine(80 / 9);
+// Módulo
+Console.WriteLine(30 % 6);
+Console.WriteLine(62 % 17);
